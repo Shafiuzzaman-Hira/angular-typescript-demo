0546791532bb39c238d7f8eadb7e65a2d67534a2
Md. Shafiuzzaman Hira <md.shafiuzzaman.hira@gmail.com>
2017-11-25 19:07:27 +0600

    Bug fixed

--- src/app/products/product-info/product-info-others.component.ts
+++ src/app/products/product-info/product-info-others.component.ts
@@ -9,16 +9,16 @@ import { Component} from '@angular/core';
 
 export class ProductInfoOthersComponent {
   snd = [
-    { id: 1, prefix: 'ABC', },
-    { id: 2, prefix: 'NCB', },
-    { id: 3, prefix: 'ASN', },
-    { id: 4, prefix: 'AED', },
+    { id: 1, prefix: 'ABC' },
+    { id: 2, prefix: 'NCB' },
+    { id: 3, prefix: 'ASN' },
+    { id: 4, prefix: 'AED' },
   ];
 
   gl = [
     { id: 1, type: 'Product Gl', code:'90999', desc:'Mudaraba Savings Deposit'},
     { id: 2, type: 'Excise Duty', code:'11123', desc:'Excise Duty'},
-    { id: 3, type: 'Profit Paid GL', code:'78956', desc:'Profit Paid on Mudaraba Savings'},,
-    { id: 4, type: 'ABC', code:'12345', desc:'Profit Paid on Mudaraba Savings'},,
+    { id: 3, type: 'Profit Paid GL', code:'78956', desc:'Profit Paid on Mudaraba Savings'},
+    { id: 4, type: 'ABC', code:'12345', desc:'Profit Paid on Mudaraba Savings'},
   ];
 }
